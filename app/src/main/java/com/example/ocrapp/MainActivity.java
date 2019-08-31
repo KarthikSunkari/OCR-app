3fbdd39ca7b680373e37bb65ad7ef41258e7d4ec
Sai Pratyush Cherukuri <pratyush11235@gmail.com>
2019-08-31 13:50:56 +0530

    Added Camera and Cropping functions

---
+++ app/src/main/java/com/example/ocrapp/MainActivity.java
@@ -0,0 +1,115 @@
+package com.example.ocrapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+
+import android.Manifest;
+import android.content.ContentValues;
+import android.content.Intent;
+import android.content.pm.ActivityInfo;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.os.Build;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import com.soundcloud.android.crop.Crop;
+
+import java.io.File;
+import java.io.IOException;
+
+public class MainActivity extends AppCompatActivity {
+    // Camera view and button
+    private Button btnCamera;
+    private ImageView capturedImage;
+
+    // for permission requests
+    public static final int REQUEST_PERMISSION = 300;
+
+    // request code for permission requests to the os for image
+    public static final int REQUEST_IMAGE = 100;
+
+    // will hold uri of image obtained from camera
+    private Uri imageUri;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btnCamera = findViewById(R.id.btnCamera);
+        capturedImage= findViewById(R.id.capturedImage);
+
+        // request permission to use the camera on the user's phone
+        if (ActivityCompat.checkSelfPermission(this.getApplicationContext(), android.Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED){
+            ActivityCompat.requestPermissions(this, new String[] {android.Manifest.permission.CAMERA}, REQUEST_PERMISSION);
+        }
+
+        // request permission to write data (aka images) to the user's external storage of their phone
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M
+                && ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE},
+                    REQUEST_PERMISSION);
+        }
+
+        // request permission to read data (aka images) from the user's external storage of their phone
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M
+                && ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.READ_EXTERNAL_STORAGE},
+                    REQUEST_PERMISSION);
+        }
+
+        btnCamera.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openCamera();
+            }
+        });
+
+    }
+    private void openCamera() {
+        ContentValues values = new ContentValues();
+        values.put(MediaStore.Images.Media.TITLE, "New Picture");
+        values.put(MediaStore.Images.Media.DESCRIPTION, "From your Camera");
+        // tell camera where to store the resulting picture
+        imageUri = getContentResolver().insert(
+                MediaStore.Images.Media.EXTERNAL_CONTENT_URI, values);
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        intent.putExtra(MediaStore.EXTRA_OUTPUT, imageUri);
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+        // start camera, and wait for it to finish
+        startActivityForResult(intent, REQUEST_IMAGE);
+    }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        // TODO Auto-generated method stub
+        super.onActivityResult(requestCode, resultCode, data);
+        // if the camera activity is finished, obtained the uri, crop it to make it square, and send it to 'Classify' activity
+        if(requestCode == REQUEST_IMAGE && resultCode == RESULT_OK) {
+            try {
+                Uri source_uri = imageUri;
+                Uri dest_uri = Uri.fromFile(new File(getCacheDir(), "cropped"));
+                // need to crop it to square image as CNN's always required square input
+                Crop.of(source_uri, dest_uri).asSquare().start(MainActivity.this);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+        else if(requestCode == Crop.REQUEST_CROP && resultCode == RESULT_OK) {
+            imageUri = Crop.getOutput(data);
+            try {
+                Bitmap bitmap = MediaStore.Images.Media.getBitmap(getContentResolver(), imageUri);
+                capturedImage.setImageBitmap(bitmap);
+                capturedImage.setRotation(capturedImage.getRotation() + 90);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+}
